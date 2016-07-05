9c3850e60aa6933a03c50b6554222e3510413ca6
Brian Bier <briansbier@gmail.com>
2016-07-05 17:40:28 -0400

    Made minor changes and used fancybox

--- public/js/assets/scripts.js
+++ public/js/assets/scripts.js
@@ -194,6 +194,32 @@ $(document).ready(function(){
        */
 
       $('.fancybox').fancybox();
+      /*
+       *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+      */
+      $('.fancybox-media')
+        .attr('rel', 'media-gallery')
+        .fancybox({
+          openEffect : 'none',
+          closeEffect : 'none',
+          prevEffect : 'none',
+          nextEffect : 'none',
 
+          arrows : false,
+          helpers : {
+            media : {},
+            buttons : {}
+          }
+        });
+
+$(".gallerypdf").fancybox({
+openEffect: 'elastic',
+closeEffect: 'elastic',
+autoSize: true,
+type: 'iframe',
+iframe: {
+preload: false // fixes issue with iframe and IE
+}
+});
   
 })
