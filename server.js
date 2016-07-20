b4d54dc4fba95d6271e487fb41efc4f988200707
Brian Bier <briansbier@gmail.com>
2016-07-19 22:38:38 -0400

    Prepare prerender for heroku

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var app = express();
 // Listening PORT
 var port = process.env.PORT || 8080;
 
-app.use(require('prerender-node').set('prerenderToken', 'w4ww74MqTwznrpvNJXSi').set('prerenderServiceUrl', 'http://localhost:3000'));
+  // Here we require the prerender middleware that will handle requests from Search Engine crawlers
+  // We set the token only if we're using the Prerender.io service
+app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
+// .set('prerenderServiceUrl', 'http://localhost:3000'))
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-  // Here we require the prerender middleware that will handle requests from Search Engine crawlers
-  // We set the token only if we're using the Prerender.io service
-// process.env.PRERENDER_TOKEN 
 //ROUTES
 require('./app/routes')(app);
 
