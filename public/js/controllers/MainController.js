75f7ceb1cdc3cc69f8f6d415ef1b50972cb786ae
Brian Bier <briansbier@gmail.com>
2016-07-22 12:55:34 -0400

    Commented out the email form

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -8,20 +8,20 @@ app.controller('MainController',['$scope','$http','services',function($scope,$ht
      return $scope.found = services.services[index]
   }
 
-  $scope.loading = false;
-  $scope.send = function(mail){
-    $scope.loading = true;
-    $http.post('/sendmail',{
-      from: mail.email,
-      to: 'briansbier@gmail.com',
-      subject: 'New Message from website user',
-      text: mail.message
-    }).then(res{
-      $scope.loading = false;
-      $scope.serverMessage = "Thank you " + mail.name + " I will be with you shortly!";
-      mail.name = ''
-      mail.email = ''
-      mail.message = ''
-    });
-  }
+  // $scope.loading = false;
+  // $scope.send = function(mail){
+  //   $scope.loading = true;
+  //   $http.post('/sendmail',{
+  //     from: mail.email,
+  //     to: 'briansbier@gmail.com',
+  //     subject: 'New Message from website user',
+  //     text: mail.message
+  //   }).then(res{
+  //     $scope.loading = false;
+  //     $scope.serverMessage = "Thank you " + mail.name + " I will be with you shortly!";
+  //     mail.name = ''
+  //     mail.email = ''
+  //     mail.message = ''
+  //   });
+  // }
 }]);
